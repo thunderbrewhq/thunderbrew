9b7ca5cf6a6e41103972765a7d5bd413479670c9
VDm <vdm-dev@users.noreply.github.com>
2025-04-05 22:47:33 +0400

    chore(db): update ItemRandomPropertiesRec

--- src/db/rec/ItemRandomPropertiesRec.hpp
+++ src/db/rec/ItemRandomPropertiesRec.hpp
@@ -9,9 +9,9 @@ class SFile;
 class ItemRandomPropertiesRec {
     public:
         int32_t m_ID;
-        const char* m_internalName;
-        int32_t m_enchantment[5];
         const char* m_name;
+        int32_t m_enchantment[5];
+        const char* m_name_lang;
 
         static const char* GetFilename();
         static uint32_t GetNumColumns();
